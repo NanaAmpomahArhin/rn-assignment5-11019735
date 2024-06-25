1094b642c97678b255eabbd8db28507031858013
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-06-24 22:02:21 -0700

    changed the default icon of the home bottom tab

--- rn-assignment5-11019735/App.js
+++ rn-assignment5-11019735/App.js
@@ -1,4 +1,5 @@
 import HomeScreen from "./components/HomeScreen";
+import { Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -8,7 +9,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                source={require("././/assets/home.png")}
+                style={{ width: size, height: size, tintColor: color }}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
