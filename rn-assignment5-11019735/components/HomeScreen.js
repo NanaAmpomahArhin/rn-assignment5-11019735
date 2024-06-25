debabfaf4a3bdd94b603fe1a18648d204925ad1f
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-06-24 20:14:11 -0700

    imported components and used them

--- rn-assignment5-11019735/components/HomeScreen.js
+++ rn-assignment5-11019735/components/HomeScreen.js
@@ -1,19 +1,30 @@
 import { View, StyleSheet } from "react-native";
 import Profile from "./Profile";
 import Search from "./Search";
+import Card from "./Card";
+import ActionIcons from "./ActionIcons";
+import Transaction from "./Transaction";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Profile />
-      <Search />
+      <View style={styles.header}>
+        <Profile />
+        <Search />
+      </View>
+      <Card />
+      <ActionIcons />
+      <Transaction />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: "colum",
+  },
+  header: {
     flexDirection: "row",
-    alignItems: "center",
   },
 });
