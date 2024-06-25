867776f7c539945a60cd94dc0e35ce21c388f309
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-06-25 07:09:46 -0700

    created a compoonent that stores the data for the settings screen

---
+++ rn-assignment5-11019735/Data/SettingsData.js
@@ -0,0 +1,22 @@
+export const settingsData = [
+  {
+    id: 0,
+    name: "Language",
+  },
+  {
+    id: 1,
+    name: "My Profile",
+  },
+  {
+    id: 2,
+    name: "Contact Us",
+  },
+  {
+    id: 3,
+    name: "Change Password",
+  },
+  {
+    id: 4,
+    name: "Privacy Policy",
+  },
+];
