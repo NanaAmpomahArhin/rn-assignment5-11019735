d64b5b1dff5eae08129c0e571bd4a6b451acffd7
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-06-24 20:10:14 -0700

    created a data directory to store all data

---
+++ rn-assignment5-11019735/Data/ActionIconsData.js
@@ -0,0 +1,22 @@
+export const actionIcons = [
+  {
+    id: 0,
+    image: require("./../assets/send.png"),
+    name: "Sent",
+  },
+  {
+    id: 1,
+    image: require("./../assets/recieve.png"),
+    name: "Recieve",
+  },
+  {
+    id: 2,
+    image: require("./../assets/loan.png"),
+    name: "Loan",
+  },
+  {
+    id: 3,
+    image: require("./../assets/topUp.png"),
+    name: "Topup",
+  },
+];
---
+++ rn-assignment5-11019735/Data/TransactionData.js
@@ -0,0 +1,30 @@
+export const transactionData = [
+  {
+    id: 0,
+    image: require("./../assets/apple.png"),
+    brandName: "Apple Store",
+    brandAction: "Entertainment",
+    cost: "-$5,99",
+  },
+  {
+    id: 1,
+    image: require("./../assets/spotify.png"),
+    brandName: "Spotify ",
+    brandAction: "Music",
+    cost: "-$12,99",
+  },
+  {
+    id: 2,
+    image: require("./../assets/moneyTransfer.png"),
+    brandName: "Money Transfer",
+    brandAction: "Transaction",
+    cost: "$300",
+  },
+  {
+    id: 3,
+    image: require("./../assets/grocery.png"),
+    brandName: "Grocery",
+    brandAction: "Shopping",
+    cost: "-$88",
+  },
+];
